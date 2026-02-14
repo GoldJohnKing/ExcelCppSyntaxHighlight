75de04610f2441c0938ab55f9e23e10ecc7a130f
Gold John King <GoldJohnKing@Live.cn>
2026-02-14 11:59:43 +0800

    refactor: create ThemeConfig class for theme management

--- cpp_highlight/config/__init__.py
+++ cpp_highlight/config/__init__.py
@@ -1,5 +1,11 @@
 """Configuration module for ExcelCppSyntaxHighlight."""
 
 from .settings import FontSettings
+from .theme import ThemeConfig, TOKEN_TYPE_NAMES, DEFAULT_THEME_COLORS
 
-__all__ = ["FontSettings"]
+__all__ = [
+    "FontSettings",
+    "ThemeConfig",
+    "TOKEN_TYPE_NAMES",
+    "DEFAULT_THEME_COLORS",
+]
